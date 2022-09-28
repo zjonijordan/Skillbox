a77b4dcd4b41a9aa5e594aad0f139acd273683ce
Home <z1@>
2022-09-28 21:15:29 +0300

    Задание 3 сделано

--- 15_IDE/03_sum_and_dif/main.py
+++ 15_IDE/03_sum_and_dif/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+##Задача 3. Сумма и разность
+N = str(input("Ведите число: "))
+def summa():
+    N1 = 0
+    for i in N:
+        N1 = int(i) + N1
+    return N1
+def summa2():
+    count = 0
+    for i in N:
+        count += 1
+    return count
+
+print("Сумма цифр: ", summa(), "\nКол-во цифр в числе: ", summa2(), "\nРазность суммы и кол-ва цифр: ", summa() - summa2() )
+
+
+

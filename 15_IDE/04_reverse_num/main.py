3a4a6f036d574cd269dd5e807d49c3d9d6797133
Home <z1@>
2022-09-30 22:48:59 +0300

    Задание 4 сделано

--- 15_IDE/04_reverse_num/main.py
+++ 15_IDE/04_reverse_num/main.py
@@ -1,3 +1,30 @@
-# TODO здесь писать код
+def change_num(num):
+    count2 = ""
+    for word in num:
+        count2 = word + count2
+    return count2
+def separate(arg):
+    j = str()
+    k = str()
+    for i in arg:
+        j += i
+        if i == ".":
+            j = j.replace(".", "")
+            break
+    j = change_num(j)
+    for i in arg:
+        if i == ".":
+            k = str()
+        else:
+            k += i
+    k = change_num(k)
+    return (str(j) + "." + str(k))
 
+first_inp = str(input("Ведите первое число: "))
+second_inp = str(input("Ведите второе число: "))
 
+first_num = float(separate(first_inp))
+print("Первое число наоборот: ", first_num)
+second_num = float(separate(second_inp))
+print("Второе число наоборот: ", second_num)
+print("Сумма: ", first_num + second_num)

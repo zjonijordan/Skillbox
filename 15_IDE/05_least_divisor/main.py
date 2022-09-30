ffc2a833a0d33a062486a4acbf96ae57d329ca61
Home <z1@>
2022-09-30 23:08:57 +0300

    Задание 5 сделано

--- 15_IDE/05_least_divisor/main.py
+++ 15_IDE/05_least_divisor/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+number = int(input("Ведите число: "))
+
+for i in range(2, number):
+    if number / i == number // i:
+        number = i
+print("Наименьший делитель, отличный от единицы:", number)

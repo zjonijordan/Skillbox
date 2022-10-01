9cd817d694613309db797b6eec537ac012e5add5
Home <z1@>
2022-10-01 10:46:18 +0300

    Задание 6 сделано

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+print("Введите координаты монетки:")
+x = float(input("x: "))
+y = float(input("y: "))
+R = float(input("Ведите радиус: "))
+
+if (x or y) >= R or (x or y) <= -R:
+    print("Монетки в области нет")
+else:
+    print("Монетка где-то рядом")

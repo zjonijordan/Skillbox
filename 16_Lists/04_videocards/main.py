41f3830a684e74a82603931a0e62ae7aec4af12c
Home <z1@>
2022-10-01 22:02:06 +0300

    Задание 4 сделано

--- 16_Lists/04_videocards/main.py
+++ 16_Lists/04_videocards/main.py
@@ -1,3 +1,14 @@
-# TODO здесь писать код
+card = int(input("Кол-во видеокарт: "))
+hight_model = "0"
+old_list = []
+new_list = []
 
-
+for i in range(1, card + 1):
+    model = str(input(str(i) + " Видеокарта: "))
+    if model > hight_model:
+        hight_model = model
+    old_list.append(model)
+for t in old_list:
+    if t != hight_model:
+        new_list.append(t)
+print("Старый список видеокарт: ", old_list, "\nНовый список видеокарт: ", new_list)

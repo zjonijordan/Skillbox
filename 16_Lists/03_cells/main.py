5f24b41df4451411e0697f47ad3d5384da82e6bc
Home <z1@>
2022-10-01 21:24:54 +0300

    Задание 3 сделано

--- 16_Lists/03_cells/main.py
+++ 16_Lists/03_cells/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+cage = int(input("Кол-во клеток: "))
+cage_new = []
+
+for i in range(1, cage + 1):
+    effect = int(input('Эффективность ' + str(i) + " клетки: "))
+    if effect < i:
+        cage_new.append(effect)
+
+print("Неподходящие значения: ", cage_new)
+
+

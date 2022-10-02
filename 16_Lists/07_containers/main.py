0e4cf2c39ac764f98eaf7a9fc4f2540e46492096
Home <z1@>
2022-10-02 20:37:45 +0300

    Задание 7 сделано

--- 16_Lists/07_containers/main.py
+++ 16_Lists/07_containers/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+quantity = int(input("Кол-во контейнеров: "))
+massa = 0
+count = 0
+kon = []
+
+while count != quantity:
+    massa = int(input("Введите вес контейнера: "))
+    if massa <= 200:
+        kon.append(massa)
+        count += 1
+    else:
+        print("Масса не может быть больше 200кг")
+
+count = 0
+new_cont = int(input("Введите вес нового контейнера: "))
+for i in kon:
+    if new_cont <= i:
+        count += 1
+print("Номер, куда встанет новый контейнер:", count)

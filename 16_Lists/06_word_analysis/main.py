c4a59bba9f07da37033f288786da1005a6a866c3
Home <z1@>
2022-10-02 20:00:04 +0300

    Задание 6 сделано

--- 16_Lists/06_word_analysis/main.py
+++ 16_Lists/06_word_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+number_word = len(word)
+new_word = list(word)
+count = 0
+
+for number in range(number_word):
+    for i in word:
+        if i == new_word[number]:
+            count += 1
+
+answer = number_word - count
+if answer < 0:
+    answer = abs(answer)
+elif answer == 0:
+    answer = number_word
+print("Кол-во уникальных букв:", answer)
+

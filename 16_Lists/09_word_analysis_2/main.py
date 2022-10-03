6eac8c5a2ccfa882d5dd2f90bb8b8950ee73ed97
Home <z1@>
2022-10-03 13:24:02 +0300

    Задание 9 сделано

--- 16_Lists/09_word_analysis_2/main.py
+++ 16_Lists/09_word_analysis_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+inverted = ""
+
+for i in word:
+    inverted = i + inverted
+
+if word == inverted:
+    print("Слово является палиндромом")
+else:
+    print("Слово не является палиндромом")

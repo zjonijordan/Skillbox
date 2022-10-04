cf3cf22e79ec5d1e322574479bbbdd2ca40f6eb8
Home <z1@>
2022-10-04 21:28:36 +0300

    Задание 10 сделано

--- 16_Lists/10_sort/main.py
+++ 16_Lists/10_sort/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+N = [1, 4, -3, 0, 10]
+Flag = True
+
+print("Изначальный список:", N)
+while Flag == True:
+    Flag = False
+    for index in range(len(N)):
+        if (index + 1 <= len(N) - 1) and (N[index] > N[index + 1]):
+            Flag = True
+            N[index], N[index + 1] = N[index + 1], N[index]
+
+print("Отсортированный список:", N)
+

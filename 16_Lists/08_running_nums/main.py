ce304b0b5717de59f2f9f018b6a09d813ebc3871
Home <z1@>
2022-10-04 23:28:45 +0300

    Задание 8 улучшено

--- 16_Lists/08_running_nums/main.py
+++ 16_Lists/08_running_nums/main.py
@@ -2,10 +2,6 @@ K = int(input("Сдвиг: "))
 string = ["1", "2", "3", "4", "5"]
 new_string = []
 
-if K == 2:
-    K = 4
-elif K == 4:
-    K = 2
 for count in range(-K, len(string) - K):
     new_string += string[count]
 print("Изначальный список:", string, "\nСдвинутый список:", new_string)

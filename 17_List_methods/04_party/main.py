624f42a870009dde60c41b2f5d333291f4eab087
Home <z1@>
2022-10-07 21:46:47 +0300

    Задание 4 выполненно

--- 17_List_methods/04_party/main.py
+++ 17_List_methods/04_party/main.py
@@ -1,3 +1,19 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+Flag = False
 
-# TODO здесь писать код
+while Flag == False:
+    guest = input("Гость пришел или ушел? ")
+    if guest == "Пришел":
+        name = input("Имя гостя: ")
+        if len(guests) >= 6:
+            print("Прости,", name, " но мест нет.")
+        else:
+            guests.append(name)
+            print("Привет,", name + "!", "\nСейчас на вечеринке", len(guests), "человек:", guests)
+    elif guest == "Ушел":
+        name = input("Имя гостя: ")
+        guests.remove(name)
+        print("Пока,", name + "!", "\nСейчас на вечеринке", len(guests), "человек:", guests)
+    elif guest == "Пора спать":
+        print("Вечеринка закончилась, все легли спать.")
+        Flag = True

5e082425d2b537077cccd0fe55d9de36e99341db
Home <z1@>
2022-10-07 16:34:59 +0300

    Задание 2 выполненно

--- 17_List_methods/02_line/main.py
+++ 17_List_methods/02_line/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+first = []
+second = []
+Flag = True
+def klass(first, beg, end, step):
+    for i in range(beg, end, step):
+        first.append(i)
+
+klass(first, 160, 177, 2)
+klass(second, 162, 181, 3)
+
+first += second
+first.sort()
+
+print("Отсортированный список:", first)

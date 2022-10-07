d71eaaeef2a14cbf674857c72a16efe1b9a7d7ae
Home <z1@>
2022-10-07 19:23:39 +0300

    Задание 1 выполненно

--- 17_List_methods/01_scary_code/main.py
+++ 17_List_methods/01_scary_code/main.py
@@ -1,24 +1,14 @@
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
-
-# TODO переписать программу
+a.extend(b)
+def func(number, temp, temp2):
+    count = 0
+    for i in a:
+        if i == number:
+            count += 1
+    print("Кол-во цифр", temp, "при", temp2, "объединении: ", count)
+func(5, 5, "первом")
+a.extend(c)
+func(3, 3, "втором")
+print(a)

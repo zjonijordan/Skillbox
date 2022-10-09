c4b0c5f46d736baef2edec40f6cfdd50087998c7
Home <z1@>
2022-10-09 17:27:29 +0300

    Задание 10 выполненно

--- 17_List_methods/10_simmetrical_seq/main.py
+++ 17_List_methods/10_simmetrical_seq/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+arr = []
+suffix = []
+buf = []
+last_i = len(arr)-1
+HM = int(input("Кол-во чисел: "))
+
+
+for t in range(HM):
+    x = int(input("Число: "))
+    arr.append(x)
+
+for i in range(len(arr)):
+    if arr[i] != arr[last_i]:
+        if len(buf):
+            suffix.extend(buf)
+            buf = []
+        suffix.insert(0, arr[i])
+    elif last_i != i:
+        buf.insert(0, arr[i])
+        last_i -= 1
+    if i == last_i:
+        break
+
+print("Последовательность:", arr, "\nНужно приписать чисел:",len(suffix),"\nСами числа:",suffix)

1992f1c109867560e9fe28e5dc2f2706ff01ef02
Home <z1@>
2022-10-09 17:56:16 +0300

    Исправлено 10

--- 17_List_methods/10_simmetrical_seq/main.py
+++ 17_List_methods/10_simmetrical_seq/main.py
@@ -1,20 +1,22 @@
 arr = []
 suffix = []
 buf = []
-last_i = len(arr)-1
 HM = int(input("Кол-во чисел: "))
-
 
 for t in range(HM):
     x = int(input("Число: "))
     arr.append(x)
 
+last_i = len(arr) - 1
+
 for i in range(len(arr)):
     if arr[i] != arr[last_i]:
         if len(buf):
-            suffix.extend(buf)
+            buf.extend(suffix)
+            suffix = buf
             buf = []
         suffix.insert(0, arr[i])
+        last_i = len(arr) - 1
     elif last_i != i:
         buf.insert(0, arr[i])
         last_i -= 1

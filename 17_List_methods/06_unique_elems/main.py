891ba57a5e74dbd21309d9573477a39ae5e6cc5c
Home <z1@>
2022-10-09 20:32:01 +0300

    Исправлено 6

--- 17_List_methods/06_unique_elems/main.py
+++ 17_List_methods/06_unique_elems/main.py
@@ -5,7 +5,11 @@ def calculate(temp, temp2):
         count_1 += 1
         first = input("Ведите " + str(count_1) + ' число из ' + temp2 +' списка чисел: ')
         list_1.append(first)
+        for t in list_1:
+            while list_1.count(t) > 1:
+                list_1.remove(t)
+
 calculate(3, "первого")
 calculate(7, "второго")
 
-print("Новый первый список с уникальными элементами: ", set(list_1))
+print("Новый первый список с уникальными элементами: ", list_1)

e515db418ada939348fa6f52bc0739582d6059f9
Home <z1@>
2022-10-16 23:40:04 +0300

    Задание 1 выполнено

--- 18_List_comprehensions/01_vowels/main.py
+++ 18_List_comprehensions/01_vowels/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+word = str(input("Введите текст: "))
+new_word = [i for i in word if i == "А" or i == "О" or i == "И" or i == "Ы" or i == "У" or i == "Е" or i == "а" or i == "о" or i == "и" or i == "ы" or i == "у" or i == "е"]
+
+print(new_word, "\nДлина списка:", len(new_word))
+
+

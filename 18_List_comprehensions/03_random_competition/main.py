0220dcd99365f5a3363a40a3cc89aa3bf2eaa463
Home <z1@>
2022-10-17 21:36:18 +0300

    Задание 3 выполнено

--- 18_List_comprehensions/03_random_competition/main.py
+++ 18_List_comprehensions/03_random_competition/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+import random
+list_1 = [round(random.triangular(5, 10), 2) for i in range(20)]
+list2 = [round(random.triangular(5, 10), 2) for t in range(20)]
+list_3 = [list_1[u] if list_1[u] >= list2[u] else list2[u] for u in range(20)]
+
+print("Первая команда: ", list_1, "\nВторая команда: ", list2, "\nПобедители тура: ", list_3)

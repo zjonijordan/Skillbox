187b98dd9dfdc53decbcf0283e31601b3a78b6ce
Home <z1@>
2022-10-18 17:54:35 +0300

    Задание 6 выполнено

--- 18_List_comprehensions/06_list_compression/main.py
+++ 18_List_comprehensions/06_list_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+N = list(input("Ведите список чисел: "))
+count = N.count("0")
+def delite(flag):
+    for i in N:
+        N.remove("0")
+    if flag == True:
+        for q in range(count):
+            N.append('0')
+    return N
+
+print("Список до удаления:", delite(True))
+print("Список после сжатия:", delite(False))
+
+

71da69531665ecd046338c88dc947b47f4e2d088
Home <z1@>
2022-10-20 22:23:26 +0300

    Задание 10 выполнено

--- 18_List_comprehensions/10_caesar_cipher/main.py
+++ 18_List_comprehensions/10_caesar_cipher/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+dict = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+count = 0
+message = input("Введите сообщение: ")
+korn = int(input("Введите сдвиг: "))
+skript = ''
+
+for i in message:
+    if i == " ":
+        skript += " "
+    for t in dict:
+        count += 1
+        if korn + count > len(dict):
+            count = count - len(dict)
+        if t == i:
+            skript += dict[count - 1 + korn]
+    count = 0
+
+print("Зашифрованное сообщение: ", skript)

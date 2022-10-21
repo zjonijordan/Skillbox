e7c9ad5fa03a304f1dbf9ea44f17dd367a54593a
Home <z1@>
2022-10-21 21:34:16 +0300

    Исправление 1 задачи

--- 18_List_comprehensions/01_vowels/main.py
+++ 18_List_comprehensions/01_vowels/main.py
@@ -1,5 +1,5 @@
 word = str(input("Введите текст: "))
-new_word = [i for i in word if i == "А" or i == "О" or i == "И" or i == "Ы" or i == "У" or i == "Е" or i == "а" or i == "о" or i == "и" or i == "ы" or i == "у" or i == "е"]
+new_word = [i for i in word for t in ["А", "О", "И", "Ы", "У", "Е", "а", "о", "и", "ы", "у", "е"] if i == t]
 
 print(new_word, "\nДлина списка:", len(new_word))
 

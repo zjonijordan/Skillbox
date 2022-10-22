2dd9c453d4ae4ab29f53a634641022ddb9166e51
Home <z1@>
2022-10-22 11:44:30 +0300

    Исправление 7 задачи

--- 18_List_comprehensions/07_two_dimensional_list/main.py
+++ 18_List_comprehensions/07_two_dimensional_list/main.py
@@ -1,2 +1,2 @@
-x = [[i for i in range(1,10,4)], [i for i in range(2,11,4)], [i for i in range(3,12,4)], [i for i in range(4,13,4)]]
+x = [[i for i in range(i + 1,10 + i,4)] for i in range(4)]
 print(x)

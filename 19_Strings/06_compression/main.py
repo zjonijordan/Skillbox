2d085a00c2606767e4b2b71a5a290c4dcdf30ad2
Home <z1@>
2022-10-25 19:03:20 +0300

    Задача 6 сделана

--- 19_Strings/06_compression/main.py
+++ 19_Strings/06_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+string = input("Введите строку: ")
+new_string = []
+temp = string[:1:]
+count = 0
+for i in string:
+    if i != temp:
+        new_string += temp + str(count)
+        count = 1
+        temp = i
+    else:
+        count += 1
+new_string += temp + str(count)
+
+print('Закодированная строка:',"".join(new_string))

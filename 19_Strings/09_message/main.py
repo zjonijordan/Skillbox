2105b3ef3d23148d20eb9357027f4e143be9a915
Home <z1@>
2022-10-30 19:28:01 +0200

    Задача 9 сделано

--- 19_Strings/09_message/main.py
+++ 19_Strings/09_message/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+text = input('Сообщение: ')
+word = ''
+reverse = ''
+def check(temp):
+    for u in ['.', ',', ';', ':', '!', '&', '?', '-', '"', ' ', '\'']:
+        if u == temp:
+            return True
+    return False
+def lenin(w):
+    if len(w):
+        return w[::-1]
+    return ''
+
+for i in text:
+    if check(i):
+        b = lenin(word)
+        if b:
+            reverse += b
+            word = ''
+        reverse += i
+    else:
+        word += i
+
+reverse += lenin(word)
+print('Новое сообщение:', reverse)

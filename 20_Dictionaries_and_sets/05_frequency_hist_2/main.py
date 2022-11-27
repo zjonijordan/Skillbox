0008f7dd6c27a971085397c47897b3ac285b5a6b
Joni <zjonijordan@ukr.net>
2022-11-27 22:19:37 +0200

    Задача 5 сделано

--- 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
+++ 20_Dictionaries_and_sets/05_frequency_hist_2/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+string = input('Введите текст: ')
+text = dict()
+invert = dict()
+
+for i in string:
+    if i in text:
+        text[i] += 1
+    else:
+        text[i] = 1
+
+print('Оригинальный словарь частот:')
+for u in text:
+    print(u[0], ':', text[u])
+
+print('Инвертированный словарь частот:')
+for u, i in text.items():
+    invert.update(dict.fromkeys([i], []))
+for o, p in text.items():
+    invert[p] += [o]
+for q, w in invert.items():
+    print(q,':', w)
+

99fd3ad851b6019c7f012bb68de85d4694aa340b
Joni <zjonijordan@ukr.net>
2022-11-28 20:07:27 +0200

    Задача 7 сделано

--- 20_Dictionaries_and_sets/07_pizza/main.py
+++ 20_Dictionaries_and_sets/07_pizza/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+all = dict()
+for _ in range(int(input("Введите кол-во заказов: "))):
+    zakaz = input(str(_ + 1) + " заказ: ").split()
+    if zakaz[0] in all:
+        for i in list(all[zakaz[0]][0]):
+            if i == zakaz[1]:
+                all[zakaz[0]][0][zakaz[1]] += int(zakaz[2])
+            else:
+                all[zakaz[0]][0].setdefault(zakaz[1], zakaz[2])
+    else:
+        all.update({zakaz[0]: [{str(zakaz[1]): int(zakaz[2])}]})
+for i in all:
+    print(i, ':')
+    for u in all[i]:
+        for p, t in u.items():
+            print(7 * ' ', str(p) + ':', t)

ceaede5ed41eb77481fc1d246040d16024c17ef0
Joni <zjonijordan@ukr.net>
2022-12-08 15:23:00 +0200

    Задача 10 сделано

--- 20_Dictionaries_and_sets/10_palindrome_again/main.py
+++ 20_Dictionaries_and_sets/10_palindrome_again/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+all = dict()
+stringer = [i for i in input('Введите строку: ')]
+one = []
+for i in stringer:
+    if i not in all:
+        all.update({i: 1})
+    else:
+        all[i] += 1
+for p in list(all.items()):
+    if p[1] == 1:
+        one += p[0]
+        all.pop(p[0])
+if ((len(one) * 100) / len(stringer)) >= 50:
+    print('Нельзя сделать палиндромом')
+else:
+    print('Можно сделать палиндромом')
+
+
+
+
+

0b7af65d1eacdc7cb1ee20a108efbb69643075f1
Joni <zjonijordan@ukr.net>
2022-12-09 12:33:55 +0200

    Исправление 2 задание

--- 20_Dictionaries_and_sets/02_geography/main.py
+++ 20_Dictionaries_and_sets/02_geography/main.py
@@ -1,18 +1,14 @@
 place = []
-
+town = dict()
 for i in range(int(input("Кол-во стран: "))):
-    place += [[i for i in input(str(i + 1) + " страна: ").split()]]
-def check(word):
-    answer = ''
-    Flag = False
-    for i in range(len(place)):
-        for u in place[i]:
-            if u == word:
-                answer = "Город " + str(u) + " расположен в стране " +  place[i][0]
-                Flag = True
-    if Flag == False:
-        answer = "По городу " + word + " данных нет."
-    return answer
-
-for i in range(3):
-    print(check(input(str(i + 1) + " город: ")))
+    place += [i for i in input(str(i + 1) + " страна: ").split()]
+    for p in place[1::]:
+        town.setdefault(p, place[0])
+    place.clear()
+for num in range(3):
+    city = input(str(num + 1) + " город: ")
+    answer = town.get(city)
+    if answer == None:
+        print('По городу ' + str(city) + 'данных нет.')
+    else:
+        print('Город '+ city + ' расположен в стране ' + str(answer))

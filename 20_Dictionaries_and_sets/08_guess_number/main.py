541a016b9b47140c335e0ecfccc61a274c97fdec
Joni <zjonijordan@ukr.net>
2022-12-10 23:50:41 +0200

    Исправление 8 задание

--- 20_Dictionaries_and_sets/08_guess_number/main.py
+++ 20_Dictionaries_and_sets/08_guess_number/main.py
@@ -1,34 +1,21 @@
-enev = set()
-not_even = set()
-Flag = False
-for i in range(1, int(input("Введите максимальное число: ")) + 1):
-    if i % 2 == 0:
-        enev.add(i)
-    else:
-        not_even.add(i)
+import random
+enter = set(i for i in range(1, int(input("Введите максимальное число: ")) + 1))
 flag = True
-question = input('Нужное число есть среди вот этих чисел:' + str(enev) + "\nОтвет Артёма: ")
-if question == 'Нет' or question == 'нет':
-    enev = not_even
-    not_even = set()
-while len(enev) > 0:
-    if question == 'Да' or question == 'да':
-        not_even = set()
-        not_even = enev.pop()
-        if len(enev) < 2:
-            question2 = input("Число которое загадал Артём:" + str(enev) + '?'+ "\nОтвет Артёма: ")
-            if question2 == 'Да' or question2 == 'да':
-                print("Число которое загадал Артём:", enev)
-                break
-            elif question2 == 'Нет' or question2 == 'нет':
-                print("Число которое загадал Артём:", not_even)
-                break
-    elif question == 'Помогите!' or question == 'помогите!':
-        print('Артём мог загадать следующие числа: ', enev)
-        break
-    elif Flag == True:
-        if question == 'Нет' or question == 'нет':
-            print("Число которое загадал Артём:", not_even)
-            break
-    Flag = True
-    question = input('Нужное число есть среди вот этих чисел:' + str(enev) + "\nОтвет Артёма: ")
+while flag == True:
+    temp = set()
+    for i in range(len(enter) // 2):
+        temp.add(random.choice([min(enter), max(enter)]))
+    user = input('Нужное число есть среди вот этих чисел: ' + str(temp) + '\n')
+    if user == "нет":
+        enter -= temp
+    if user == "да":
+        enter = temp
+    if user == "Помогите":
+        enter -= temp
+        print('Артём мог загадать следующие числа: ', enter)
+        flag = False
+    if len(enter) < 2:
+        print("Загаданное число:", enter)
+        flag = False
+
+

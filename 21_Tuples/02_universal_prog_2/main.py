dbe55e48e5fb036ff4e75bdc8d4035075567ca87
Joni <zjonijordan@ukr.net>
2022-12-11 21:39:31 +0200

    Задание 2 сделано!

--- 21_Tuples/02_universal_prog_2/main.py
+++ 21_Tuples/02_universal_prog_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+def is_prime(x):
+    for i in range(2, (x // 2) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+data = {random.randrange(1, 100):random.choice(['win', 'lose', 'draw']) for i in range(10)}
+print({index: name for index, name in data.items() if is_prime(index) == True})

7cce5b3917fa7f3917f668281c6bc6329a20121f
Joni <zjonijordan@ukr.net>
2022-12-11 19:04:30 +0200

    Задание 6 сделано!

--- 21_Tuples/06_pairs/main.py
+++ 21_Tuples/06_pairs/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import random
+original = []
+modified = []
+for _ in range(10):
+    original += str(random.randrange(10))
+print('Оригинальный список:', original)
+for index in range(len(original)):
+    if index % 2 == 0:
+        modified.append(tuple(original[index]) + tuple(original[index + 1]))
+print('Новый список:', modified)

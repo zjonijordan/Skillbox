e9906334b2409af3cac5b0040758badd1264ed99
Joni <zjonijordan@ukr.net>
2022-12-11 19:04:30 +0200

    Задание 6 сделано!

--- 21_Tuples/06_pairs/main.py
+++ 21_Tuples/06_pairs/main.py
@@ -6,5 +6,5 @@ for _ in range(10):
 print('Оригинальный список:', original)
 for index in range(len(original)):
     if index % 2 == 0:
-        modified.append(tuple(original[index]) + tuple(original[index + 1]))
+        modified.append(tuple(original[index] + original[index + 1]))
 print('Новый список:', modified)

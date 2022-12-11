d6d78333bdd5ae2aca68bd3baf50bda81de3b504
Joni <zjonijordan@ukr.net>
2022-12-11 18:06:14 +0200

    Задание 5 сделано!

--- 21_Tuples/05_one_family/main.py
+++ 21_Tuples/05_one_family/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+BD = {'Сидоров Никита': 35,
+      'Сидорова Алина': 34,
+      'Сидоров Павел': 10,
+      'Петров Саша': 23,
+      'Петрова Анна': 23,
+      'Петровский Ваня': 18}
+enter = input('Введите фамилию: ')
+age_family = 0
+for name, age in BD.items():
+      if enter.lower() == (name[0:len(enter)]).lower():
+            age_family += age
+if age_family == 0:
+      print("Sorry, фамилия, " + str(enter) + " отсутствует в базе данных")
+else:
+      print("Сумарный возраст семьи составляет:", age_family)

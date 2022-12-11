67a995d8088f5d838155f4bc7f1cb97102142c6a
Joni <zjonijordan@ukr.net>
2022-12-11 22:17:32 +0200

    Задание 3 сделано!

--- 21_Tuples/03_function/main.py
+++ 21_Tuples/03_function/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+random_turple = tuple(('h e l l o w o r l d').split(' '))
+elem = input("enter: ")
+count = []
+for index, name in enumerate(random_turple):
+    if elem == name:
+        count += [str(index)]
+if len(count) == 0:
+    print('Нету такого элемента!')
+if len(count) == 1:
+    print(random_turple[int(count[0]):])
+if len(count) == 2:
+    print(random_turple[int(count[0]): int(count[1]) + 1])
+if len(count) > 2:
+    print(random_turple[int(count[0]): int(count[-1]) + 1])
+

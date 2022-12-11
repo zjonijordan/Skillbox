510bd0232fb6c7c862ce0205ee17e79d0dc6e714
Joni <zjonijordan@ukr.net>
2022-12-11 17:31:46 +0200

    Задание 4 сделано

--- 21_Tuples/04_players/main.py
+++ 21_Tuples/04_players/main.py
@@ -3,5 +3,7 @@ players = {
     ("Bob", "Robbin"): (7, 5, 14),
     ("Rob", "Bobbin"): (12, 8, 2)
 }
-
-# TODO здесь писать код
+perem = list()
+for name, score in players.items():
+    perem += [tuple(name[0:] + tuple(score)[0:])]
+print(perem)

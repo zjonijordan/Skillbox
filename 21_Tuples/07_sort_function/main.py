68aa087cb900d9ed796e8549de33d4f57f82859c
Joni <zjonijordan@ukr.net>
2022-12-12 19:14:19 +0200

    Задание 7 сделано!

--- 21_Tuples/07_sort_function/main.py
+++ 21_Tuples/07_sort_function/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+strin = input("Ведите числа через пробел: ")
+
+flag = True
+for i in strin:
+    if i == ".":
+        print(tuple(strin.split()), '\nЕсть дробное число')
+        flag = False
+def sort(nums):
+    nums = nums.split()
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+    return tuple(nums)
+
+if flag == True:
+    print(sort(strin))

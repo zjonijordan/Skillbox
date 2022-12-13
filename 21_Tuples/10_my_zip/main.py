babf4bbdb744a1becb052d9572295762cdab50d5
Joni <zjonijordan@ukr.net>
2022-12-13 19:05:54 +0200

    Задание 10 сделано!

--- 21_Tuples/10_my_zip/main.py
+++ 21_Tuples/10_my_zip/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+string = [i for i in 'abcd']
+qsq = (10, 20, 30, 40)
+
+new_zip = ((name, str(qsq[index])) for index, name in enumerate(string))
+
+print(new_zip)
+for i in new_zip:
+    print(i)
+

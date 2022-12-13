b44e7f1d7b43cf32d286eff94858bb771a35662e
Joni <zjonijordan@ukr.net>
2022-12-13 21:07:13 +0200

    Исправлено 3 задание

--- 21_Tuples/03_function/main.py
+++ 21_Tuples/03_function/main.py
@@ -3,13 +3,13 @@ elem = input("enter: ")
 count = []
 for index, name in enumerate(random_turple):
     if elem == name:
-        count += [str(index)]
+        count.append(index)
 if len(count) == 0:
     print('Нету такого элемента!')
 if len(count) == 1:
-    print(random_turple[int(count[0]):])
+    print(random_turple[count[0]:])
 if len(count) == 2:
-    print(random_turple[int(count[0]): int(count[1]) + 1])
+    print(random_turple[count[0]: count[1] + 1])
 if len(count) > 2:
-    print(random_turple[int(count[0]): int(count[-1]) + 1])
+    print(random_turple[count[0]: count[-1] + 1])
 

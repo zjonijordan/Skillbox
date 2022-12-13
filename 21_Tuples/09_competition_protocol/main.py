84252c381903514c01ce37ece47ed3f11a097722
Joni <zjonijordan@ukr.net>
2022-12-13 14:24:14 +0200

    Задание 9 сделано!

--- 21_Tuples/09_competition_protocol/main.py
+++ 21_Tuples/09_competition_protocol/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+iteration = int(input('Сколько записей вносится в протокол? '))
+print('Записи (результат и имя):')
+def yes_or_no():
+    for index, name in enumerate(resolt):
+        if int(resolt[index][0]) <= int(ask[0]):
+            return index
+        break
+
+for i in range(iteration):
+    ask = input(str(i + 1) + " запись: ").split()
+    if i == 0:
+        resolt = [tuple(ask)]
+    else:
+        index = yes_or_no()
+        if index != None:
+            resolt.insert(int(index), tuple(ask))
+        else:
+            resolt += [tuple(ask)]
+
+print('\nИтоги соревнований:')
+for index, name in enumerate(resolt):
+    print(str(index + 1) + ' место. ' + name[1] + " с результатом " + name[0])

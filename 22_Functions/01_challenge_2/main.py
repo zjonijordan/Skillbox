a4a13b33c541f70b450df01621675cbc72bb5c02
Joni <zjonijordan@ukr.net>
2022-12-14 19:40:20 +0200

    Задание 1 выполнено

--- 22_Functions/01_challenge_2/main.py
+++ 22_Functions/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+N = int(input("Enter: "))
+count = 0
+def calculate(enter, number):
+    if enter != number:
+        number += 1
+        print(number)
+        calculate(enter, number)
+        return number
+
+calculate(N, count)

5ad5ffc47bf60177d47f2b22eca2469b45eb24fd
Joni <zjonijordan@ukr.net>
2022-12-15 11:29:44 +0200

    Задание 2 выполнено

--- 22_Functions/02_my_zip_2/main.py
+++ 22_Functions/02_my_zip_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+a = [1, 2, 3]
+b = '4567'
+c = (7, 8, 9)
+def zip(*args):
+    sequences = [list(arg) for arg in args]
+    min_length = len(min(args, key = len))
+    return [tuple(seq[i] for seq in sequences) for i in range(min_length)]
+
+print(zip(a, b, c))

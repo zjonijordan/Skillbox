14a1cb057f5b13ff6d9e1a63a90b9e9aff088ed1
Joni <zjonijordan@ukr.net>
2022-12-17 14:18:53 +0200

    Задание 3 выполнено

--- 22_Functions/03_fibonacci/main.py
+++ 22_Functions/03_fibonacci/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+enter = int(input('Введите позицию числа в ряде Фибоначчи: '))
+data = {0: 0, 1: 1}
+def calculate(n):
+    if n in data:
+        return data[n]
+    data[n] = calculate(n - 1) + calculate(n - 2)
+    return data[n]
+
+print(calculate(enter))

1de1d2f3aa4874f8a49565a63eb4e32d0c7e7adc
Joni <zjonijordan@ukr.net>
2022-12-18 22:55:51 +0200

    Задание 4 выполнено

--- 22_Functions/04_search_element_2/main.py
+++ 22_Functions/04_search_element_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+site = {'html': {'head': {'title': 'Мой сайт'},'body': {'h2': 'Здесь будет мой заголовок',
+			'div': 'Тут, наверное, какой-то блок',
+			'p': 'А вот здесь новый абзац'}}}
+
+enter = input('enter: ')
+level = int(input("level(1-3): "))
+def search(web, count, deep, key):
+	if level == count:
+		print(web.get(key))
+	for i in web.values():
+		if type(i) == dict:
+			search(i, count + 1, deep, key)
+
+search(site, 1, level, enter)
